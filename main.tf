b840d968cdffea6e3ecc1d25dc16fdd7b62aabd6
federico toledo <federico.toledo@nebulan.cl>
2023-11-22 14:51:24 -0300

    cambio de instancia

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 ##Create and bootstrap webserver #lanzar
-resource "aws_instance" "webserver" {
+resource "aws_instance" "web" {
   ami                         = "ami-00c39f71452c08778"
   instance_type               = "t2.micro"
   #key_name                    = app-ssh-key 
