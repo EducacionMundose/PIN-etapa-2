35237e2baeff0cc9fce10766462b5afdf20df4ff
German <powit10@MacBook-Pro-de-PowIT10.local>
2024-12-26 15:16:16 -0300

    fix

--- setup.tf
+++ setup.tf
@@ -104,6 +104,33 @@ resource "aws_instance" "webserver" {
   }
 }
 
+resource "aws_s3_bucket" "my-bucket-mundoe" {
+  bucket = "my-bucket-mundoe"
+  acl    = "private"
+
+  tags = {
+    Name        = "MiAplicacionBucket"
+    Environment = "Production"
+  }
+}
+
+resource "aws_dynamodb_table" "my-table-mundoe" {
+  name         = "my-table-mundoe"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "MiAplicacionTabla"
+    Environment = "Production"
+  }
+}
+
 # Output de la IP pública de la instancia EC2
 output "Webserver-Public-IP" {
   value = aws_instance.webserver.public_ip
